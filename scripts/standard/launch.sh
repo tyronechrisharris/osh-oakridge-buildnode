e821315e964abbe68d83d248e681b2ac59a36179
Drew Botts <drew@Pine.lan>
2024-11-04 17:02:28 -0500

    added scripts and updated launch to encrypt login credentials

--- scripts/standard/launch.sh
+++ scripts/standard/launch.sh
@@ -4,6 +4,16 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 "$SCRIPT_DIR/load_trusted_certs.sh"
 
+# After copying the default configuration file, also look to see if they
+# specified what they want the initial admin user's password to be, either
+# as a secret file or by providing it as an environment variable.
+if [ -z "$INITIAL_ADMIN_PASSWORD_FILE" ] && [ -z "$INITIAL_ADMIN_PASSWORD" ]; then
+  export INITIAL_ADMIN_PASSWORD=admin
+fi
+"$SCRIPT_DIR/set-initial-admin-password.sh"
+
+
+
 # Start the node
 java -Xmx2g \
 	-Dlogback.configurationFile=./logback.xml \
@@ -13,4 +23,4 @@ java -Xmx2g \
 	-Djavax.net.ssl.keyStorePassword="atakatak" \
 	-Djavax.net.ssl.trustStore="$SCRIPT_DIR/trustStore.jks" \
 	-Djavax.net.ssl.trustStorePassword="changeit" \
-	org.sensorhub.impl.SensorHub ./config.json ./db
+	com.botts.impl.security.SensorHubWrapper ./config.json ./db
