5df125da37a6c24068bc009a8949aa8f13a94b86
Cardy2 <cardy2@gmail.com>
2024-11-11 17:01:31 -0500

    Write set-initial-admin-password.bat; Update launch.bat to implement this, allowing password encoding and updating the config.json with newly encoded password

--- scripts/standard/launch.bat
+++ scripts/standard/launch.bat
@@ -1,8 +1,18 @@
 @echo off
-setlocal
+setlocal enabledelayedexpansion
+
 
 REM Make sure all the necessary certificates are trusted by the system.
 CALL %~dp0load_trusted_certs.bat
+
+REM Check if INITIAL_ADMIN_PASSWORD_FILE and INITIAL_ADMIN_PASSWORD are empty
+REM Set default password if neither is provided
+if "%INITIAL_ADMIN_PASSWORD_FILE%"=="" if "%INITIAL_ADMIN_PASSWORD%"=="" (
+    set INITIAL_ADMIN_PASSWORD=admin
+)
+
+REM Call the next batch script to handle setting the initial admin password
+CALL "%SCRIPT_DIR%set-initial-admin-password.bat"
 
 REM Start the node
 java -Xmx2g ^
